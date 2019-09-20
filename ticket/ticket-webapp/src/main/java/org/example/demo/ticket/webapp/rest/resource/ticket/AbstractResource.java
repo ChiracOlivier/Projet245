dcd2ea3a88880f9adc4f38dc5b61fe5100a262e7
Chiracolivier <chiracolivier28@gmail.com>
2019-09-21 01:46:50 +0200

    add AbstractResource in Ticket-webapp

---
+++ ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/rest/resource/ticket/AbstractResource.java
@@ -0,0 +1,17 @@
+package org.example.demo.ticket.webapp.rest.resource.ticket;
+
+import org.example.demo.ticket.business.manager.impl.ManagerFactory;
+
+public abstract class AbstractResource {
+
+    private static ManagerFactory managerFactory;
+
+    protected static  ManagerFactory getManagerFactory(){
+        return managerFactory;
+    }
+
+    public static void setManagerFactory(ManagerFactory pManagerFactory){
+        managerFactory=pManagerFactory;
+    }
+
+}
