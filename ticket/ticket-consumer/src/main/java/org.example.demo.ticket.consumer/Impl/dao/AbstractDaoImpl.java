bfb0d5c18a54f6a5c826633d65e7d3fa6103bac3
Chiracolivier <chiracolivier28@gmail.com>
2019-09-22 23:31:44 +0200

    add content to AbstractDaoImpl in Ticket-Consumer

--- ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/Impl/dao/AbstractDaoImpl.java
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/Impl/dao/AbstractDaoImpl.java
@@ -1,5 +1,5 @@
 package org.example.demo.ticket.consumer.Impl.dao;
 
 public class AbstractDaoImpl {
-    
+
 }
--- ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/Impl/dao/TicketDaoImpl.java
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/Impl/dao/TicketDaoImpl.java
@@ -2,7 +2,16 @@ package org.example.demo.ticket.consumer.Impl.dao;
 
 import org.example.demo.ticket.consumer.contract.dao.TicketDao;
 
-public class TicketDaoImpl implements TicketDao {
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.sql.DataSource;
 
+public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao {
 
+    @Inject
+    @Named("dataSourceTicket")
+    private DataSource dataSource;
+    protected DataSource getDataSource() {
+        return dataSource;
+    }
 }
