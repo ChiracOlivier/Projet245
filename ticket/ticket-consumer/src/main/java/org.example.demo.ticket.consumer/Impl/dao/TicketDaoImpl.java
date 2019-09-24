73088a7f85a75510ce61a4c38c4461e825411e87
Chiracolivier <chiracolivier28@gmail.com>
2019-09-24 09:28:26 +0200

    add content to TicketDaoImpl.java

--- ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/Impl/dao/TicketDaoImpl.java
+++ ticket/ticket-consumer/src/main/java/org.example.demo.ticket.consumer/Impl/dao/TicketDaoImpl.java
@@ -5,5 +5,13 @@ import org.example.demo.ticket.consumer.contract.dao.TicketDao;
 
 public class TicketDaoImpl extends AbstractDaoImpl implements TicketDao {
 
+    @Override
+    public int getCountTicket(RechercheTicket pRechercheTicket) {
+        JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
+        int vNbrTicket = vJdbcTemplate.queryForObject(
+                "SELECT COUNT(*) FROM ticket",
+                Integer.class);
 
+        return vNbrTicket;
+    }
 }
